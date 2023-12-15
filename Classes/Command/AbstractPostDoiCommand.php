741a0a4ba9f16aaadd4c568879b3ce9786939e42
Philipp Kitzberger <coding@kitze.net>
2023-12-15 13:22:37 +0100

    Add options to define query constraints

--- Classes/Command/AbstractPostDoiCommand.php
+++ Classes/Command/AbstractPostDoiCommand.php
@@ -2,10 +2,12 @@
 
 namespace Kitzberger\PowermailAdvancedDoi\Command;
 
+use Doctrine\DBAL\Connection;
 use In2code\Powermail\Domain\Model\Mail;
 use In2code\Powermail\Domain\Repository\MailRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use TYPO3\CMS\Core\Database\ConnectionPool;
@@ -14,6 +16,9 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 abstract class AbstractPostDoiCommand extends Command
 {
+    protected const DESCRIPTION = 't.b.c.';
+    protected const TYPE = null;
+
     /**
      * @var SymfonyStyle
      */
@@ -26,7 +31,22 @@ abstract class AbstractPostDoiCommand extends Command
 
     protected function configure()
     {
-        $this->setDescription('t.b.d.');
+        $this->setDescription(static::DESCRIPTION);
+
+        $this->addOption(
+            'type',
+            't',
+            InputOption::VALUE_REQUIRED,
+            'Handle only records of this type',
+            static::TYPE
+        );
+
+        $this->addOption(
+            'pids',
+            'p',
+            InputOption::VALUE_REQUIRED,
+            'Handle only records within these pids'
+        );
     }
 
     /**
@@ -40,6 +60,8 @@ abstract class AbstractPostDoiCommand extends Command
         $this->io = new SymfonyStyle($input, $output);
 
         $this->conf = $input->getArguments();
+        $this->conf['type'] = $input->getOption('type');
+        $this->conf['pids'] = $input->getOption('pids');
 
         if ($output->isVerbose()) {
             $this->io->title($this->getDescription());
@@ -47,10 +69,23 @@ abstract class AbstractPostDoiCommand extends Command
 
         $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tx_powermailadvanceddoi_postdoiaction');
         $queryBuilder->getRestrictions()->removeAll()->add(GeneralUtility::makeInstance(DeletedRestriction::class));
+
+        $constraints = [];
+        $constraints[] = $queryBuilder->expr()->eq('done_at', $queryBuilder->createNamedParameter(0, \PDO::PARAM_INT));
+
+        if ($this->conf['type'] ?? false) {
+            $constraints[] = $queryBuilder->expr()->eq('type', $queryBuilder->createNamedParameter($this->conf['type'], \PDO::PARAM_STR));
+        }
+
+        if ($this->conf['pids'] ?? false) {
+            $pids = GeneralUtility::intExplode(',', $this->conf['pids'], true);
+            $constraints[] = $queryBuilder->expr()->in('pid', $queryBuilder->createNamedParameter($pids, Connection::PARAM_INT_ARRAY));
+        }
+
         $postDoiActions = $queryBuilder
             ->select('*')
             ->from('tx_powermailadvanceddoi_postdoiaction')
-            ->where($queryBuilder->expr()->eq('done_at', $queryBuilder->createNamedParameter(0, \PDO::PARAM_INT)))
+            ->where($queryBuilder->expr()->andX(...$constraints))
             ->executeQuery()->fetchAll();
 
         $mailRepository = GeneralUtility::makeInstance(MailRepository::class);
